36c24dcd1b47755a74ffc68d0e80132310a212c0
borislavr <boris.lavr@gmail.com>
2025-03-26 16:48:08 +0300

    Add multi-module Maven release action scripts and workflow configuration

---
+++ .github/actions/maven-multimodule-release/action-scripts.sh
@@ -0,0 +1,73 @@
+#! /usr/bin/env bash
+
+check_version_type() {
+    if [[ $1 != "major" && $1 != "minor" && $1 != "patch" ]]; then
+        echo "Invalid version type. Please use major, minor or patch."
+        exit 1
+    fi
+}
+
+check_module() {
+    if [ -z "$1" ]; then
+        echo "Module is required."
+        exit 1
+    fi
+}
+
+check_token() {
+    if [ -z "$1" ]; then
+        echo "token input parameter is required."
+        exit 1
+    fi
+}
+
+set_env_vars() {
+    export MODULE=$1
+    export VERSION_TYPE=$2
+}
+
+bump_version_and_build() {
+    cd ${GITHUB_WORKSPACE}/${MODULE}
+    git config --global user.name "${{ github.actor }}"
+    git config --global user.email "${{ github.actor }}@users.noreply.github.com"
+    echo "Bumping ${MODULE} version"
+    if [ "${VERSION_TYPE}" == "major" ]; then
+        mvn build-helper:parse-version versions:set -DgenerateBackupPoms=false \
+        -DnewVersion=\${parsedVersion.nextMajorVersion}.0.0
+    elif [ "${VERSION_TYPE}" == "minor" ]; then
+        mvn build-helper:parse-version versions:set -DgenerateBackupPoms=false \
+        -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.nextMinorVersion}.0
+    else
+        mvn build-helper:parse-version versions:set -DgenerateBackupPoms=false \
+        -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}
+    fi
+    export VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+    echo "VERSION=${VERSION}" >> $GITHUB_ENV
+    echo "Building ${MODULE} version ${VERSION}"
+    mvn install
+    mvn clean
+    git add .
+    git commit -m "Bump version to ${VERSION} [skip ci]"
+    git push
+    git tag -a v${VERSION} -m "Release version v${VERSION} [skip ci]"
+    git push origin v${VERSION}
+}
+
+maven_deploy() {
+    cd ${GITHUB_WORKSPACE}/${MODULE}
+    export VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+    echo "Deploying ${MODULE} version ${VERSION}"
+    # mvn --batch-mode deploy -DskipTests
+    mvn --batch-mode install -DskipTests
+}
+
+bump_to_next_snapshot() {
+    cd ${GITHUB_WORKSPACE}/${MODULE}
+    mvn build-helper:parse-version versions:set -DgenerateBackupPoms=false \
+    -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}-SNAPSHOT
+    export VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+    mvn clean
+    git add .
+    git commit -m "Bump version to next snapshot ${VERSION} [skip ci]"
+    git push
+}
